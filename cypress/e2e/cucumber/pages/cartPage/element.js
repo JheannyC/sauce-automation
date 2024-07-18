6fce61d9a552d096aae39a761c8de05862cf0a8b
Jeane Cardoso <jeane.cardoso@match.mt>
2024-07-17 22:30:27 -0300

    task: testes da página de carrinho

---
+++ cypress/e2e/cucumber/pages/cartPage/element.js
@@ -0,0 +1,26 @@
+const element = {
+    attr: {
+
+        cartLabel: '.subheader',
+        backButton:'.inventory_details_back_button',
+        addToCart: '.btn_primary',
+        removeCartButton: '.btn_secondary',
+        cartInventoryDetail: '.inventory_details_name',
+        productsCounter: '.fa-layers-counter',
+        cartIconButton: '#shopping_cart_container',
+        checkoutButton: '.checkout_button',
+    },
+
+    label: {
+        addToCart: 'ADD TO CART',
+        remove: 'REMOVE',
+        checkout: 'CHECKOUT'
+    },
+
+    route: {
+        cartUrl: '/v1/cart.html'
+    },
+
+}
+
+export default element
