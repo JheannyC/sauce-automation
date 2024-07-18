0020bc71504c798ac81bc00482090ea6e15f1154
Jeane Cardoso <jeane.cardoso@match.mt>
2024-07-17 22:28:38 -0300

    task: testes da página de produtos

---
+++ cypress/e2e/cucumber/pages/productsPage/element.js
@@ -0,0 +1,28 @@
+const element = {
+    /*
+        Use a pattern to register ids;
+        bug: burguerMenu is not working. Cypress can not identify.
+    */
+
+    attr: {
+        header: '#header_container',
+        productLabel: '.product_label',
+        burguerMenu: '.bm-burger-button > button',
+        appLogo: '.app_logo',
+        cart: '#shopping_cart_container',
+        productsList: '.inventory_list',
+        inventoryImage: '.inventory_item_img',
+        inventoryItemName: '.inventory_item_name',
+        inventoryItemDescription: '.inventory_item_desc',
+        inventoryItemPrice: '.inventory_item_price',
+        cartButton: '.btn_primary',
+        productId1: '#item_1_title_link',
+        productId2: '#item_2_title_link',
+        productNameDetail: '.btn_primary'
+    },
+
+
+}
+
+
+export default element
