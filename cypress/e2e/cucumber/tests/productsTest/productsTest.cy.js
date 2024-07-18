6d075a3f4ae40802ef26bd0dabf56f454f1b3a0d
Jeane Cardoso <jeane.cardoso@match.mt>
2024-07-18 01:58:27 -0300

    wip: checkout page

---
+++ cypress/e2e/cucumber/tests/productsTest/productsTest.cy.js
@@ -0,0 +1,27 @@
+import {Given, When, Then, And} from 'cypress-cucumber-preprocessor/steps'
+import loginPage from '../../pages/loginPage/LoginPage.cy'
+import productsPage from '../../pages/productsPage/ProductsPage.cy'
+
+Given('I login with a valid username and password', () => {
+    loginPage.login(
+        `${Cypress.env('USERNAME')}`,
+        `${Cypress.env('PASSWORD')}`
+    )
+})
+
+When('access the product page', () => {
+    productsPage.checkAllHeaderElements()
+})
+
+When('choose product by name', () => {
+    productsPage.addproductByIdToCart(4, 'Sauce Labs Backpack')
+})
+
+
+Then('the products list is displayed with image, name, price and description', ()=> {
+    productsPage.checkProductsList()
+})
+
+Then('click in add to cart', () => {
+    productsPage.clickAddToCartButton()
+})
